60ebf16f334f580239bc028e8c0755edfd077cfc
Chris Neal <chrisvneal@gmail.com>
2024-09-23 23:38:09 -1000

    Add axios

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-let names = ["Michael", "Manny"];
+const API_KEY = "08a2729c1ca5722f668f14f4c28e04c4";
+const baseURL = "https://superheroapi.com/api/";
+
+let names = [];
 
 app.get("/", (req, res) => {
-	res.render("index.ejs", { title: "Hero Connect", names });
+	getData(req, res);
 });
+
+let getData = async (req, res) => {
+	try {
+		const response = await axios.get(`${baseURL}${API_KEY}/23`);
+		const data = response.data;
+		names.push(data.name);
+		res.render("index.ejs", { title: "Hero Connect", names });
+	} catch (error) {
+		console.error(`There was an error: ${error}`);
+	}
+};
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);

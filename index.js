e386eb2f90bf6c53e3cfa68e35b4b041f964c9f0
Chris Neal <chrisvneal@gmail.com>
2024-09-24 11:02:19 -1000

    Create character search form

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const url = `${baseURL}${API_KEY}/`;
 
 app.get("/", async (req, res) => {
 	try {
+		// on default load, start with random character selected
 		const randomHeroID = Math.floor(Math.random() * 732 + 1);
 
 		const characterData = await getCharacterData(randomHeroID);
@@ -24,9 +25,37 @@ app.get("/", async (req, res) => {
 	}
 });
 
-let getCharacterData = async (heroId) => {
-	const response = await axios.get(`${url}${heroId}`);
-	return response.data;
+app.post("/herosearch", async (req, res) => {
+	try {
+		const searchedName = req.body.heroname;
+		const characterData = await getCharacterData(searchedName);
+
+		res.render("index.ejs", {
+			title: "Home Connect",
+			data: characterData,
+		});
+	} catch (error) {
+		console.error(`There was an error: ${error}`);
+		res.status(500).send("An error occurred while searching for the hero.");
+	}
+});
+
+let getCharacterData = async (character) => {
+	let response;
+
+	// if users text it should be concatenated with "search"
+	if (isNaN(character)) {
+		character = `search/${character}`;
+
+		response = await axios.get(`${url}${character}`);
+		response = response.data.results[0];
+	} else {
+		// if the function receives an 'id', retrieve from data.results
+		response = await axios.get(`${url}${character}`);
+		response = response.data;
+	}
+
+	return response;
 };
 
 app.listen(port, () => {

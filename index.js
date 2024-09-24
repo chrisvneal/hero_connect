cc4e1c0ac4329f5db371b7f3ba4fbf15b5cb9fa5
Chris Neal <chrisvneal@gmail.com>
2024-09-24 08:38:59 -1000

    Revised getCharacterData function

--- index.js
+++ index.js
@@ -15,22 +15,19 @@ const url = `${baseURL}${API_KEY}/`;
 
 let names = [];
 
-app.get("/", (req, res) => {
-	// getData(req, res);
-	res.render("index.ejs", { title: "Home Connect" });
-});
-
-// let characterData;
-
-let getCharacterData = async (req, res) => {
+app.get("/", async (req, res) => {
 	try {
-		const response = await axios.get(`${url}${25}`);
-		const characterData = response.data;
-		// names.push(data.name);
-		res.render("index.ejs", { title: "Hero Connect", characterData });
+		const characterData = await getCharacterData();
+		res.render("index.ejs", { title: "Hero Connect", data: characterData });
 	} catch (error) {
 		console.error(`There was an error: ${error}`);
 	}
+});
+
+let getCharacterData = async () => {
+	const randomHeroID = Math.floor(Math.random() * 732 + 1);
+	const response = await axios.get(`${url}${randomHeroID}`);
+	return response.data;
 };
 
 app.listen(port, () => {
